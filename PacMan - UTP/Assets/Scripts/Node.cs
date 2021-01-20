d95a1b7ac6ec58dc53d6a8badbcc8f45bd2408b1
dawsta006 <dawsta006@o365.student.utp.edu.pl>
2021-01-20 19:20:11 +0100

    adding pacman animation and basics of movement

--- PacMan - UTP/Assets/Scripts/Node.cs
+++ PacMan - UTP/Assets/Scripts/Node.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+
+    public Node[] neighbors;
+    public Vector2[] validDirections;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        validDirections = new Vector2[neighbors.Length];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //computing direction vectors for neighbor nodes
+        for (int i=0; i < neighbors.Length; i++)
+        {
+            Node neighbor = neighbors[i];
+            Vector2 temp = neighbor.transform.localPosition - transform.localPosition;
+            validDirections[i] = temp.normalized;                                       
+        }
     }
+     
 }
--- PacMan - UTP/Assets/Scripts/PacMan.cs
+++ PacMan - UTP/Assets/Scripts/PacMan.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PacMan : MonoBehaviour
 {
+    public float speed = 4.0f;
+
+    private Vector2 direction = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,59 @@ public class PacMan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+        Move();
+        UpdateOrientation();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = Vector2.down;
+        }
+    }
+
+    void Move()
+    {
+        transform.localPosition += (Vector3)direction * speed * Time.deltaTime;
+    }
+
+    void UpdateOrientation()
+    {
+        if(direction == Vector2.left) {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            var newScale = transform.localScale;
+            newScale.x = -1;
+
+            transform.localScale = newScale;
+        }
+        else if (direction == Vector2.right) {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            var newScale = transform.localScale;
+            newScale.x = 1;
+
+            transform.localScale = newScale;
+        }
+        else if (direction == Vector2.up) {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 90);
+        }
+        else if (direction == Vector2.down) {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, -90);
+        }
     }
 }
